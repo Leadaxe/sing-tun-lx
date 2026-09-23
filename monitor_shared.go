3e03774a6a620ca59dbf1edbae546c16d7ad1a44
世界 <i@sekai.icu>
2026-09-23 13:45:23 +0800

    Fix IPv6 temporary address rotation reported as default interface change

--- monitor_shared.go
+++ monitor_shared.go
@@ -4,10 +4,13 @@ package tun
 
 import (
 	"errors"
+	"net/netip"
+	"slices"
 	"sync"
 	"sync/atomic"
 	"time"
 
+	"github.com/sagernet/sing/common"
 	"github.com/sagernet/sing/common/control"
 	"github.com/sagernet/sing/common/logger"
 	"github.com/sagernet/sing/common/x/list"
@@ -151,4 +154,31 @@ func (m *defaultInterfaceMonitor) MyInterfaces() []string {
 	m.access.Lock()
 	defer m.access.Unlock()
 	return m.myInterfaces
+}
+
+func defaultInterfaceChanged(oldInterface *control.Interface, newInterface *control.Interface) bool {
+	if oldInterface == nil {
+		return true
+	}
+	if oldInterface.Index != newInterface.Index ||
+		oldInterface.MTU != newInterface.MTU ||
+		oldInterface.Name != newInterface.Name ||
+		!slices.Equal(oldInterface.HardwareAddr, newInterface.HardwareAddr) ||
+		oldInterface.Flags != newInterface.Flags {
+		return true
+	}
+	oldNetworks := interfaceNetworks(oldInterface.Addresses)
+	newNetworks := interfaceNetworks(newInterface.Addresses)
+	return len(oldNetworks) != len(newNetworks) || !common.All(oldNetworks, func(it netip.Prefix) bool {
+		return slices.Contains(newNetworks, it)
+	})
+}
+
+func interfaceNetworks(addresses []netip.Prefix) []netip.Prefix {
+	return common.Uniq(common.Map(addresses, func(it netip.Prefix) netip.Prefix {
+		if !it.Addr().Is6() {
+			return it
+		}
+		return netip.PrefixFrom(it.Addr(), min(it.Bits(), 64)).Masked()
+	}))
 }
